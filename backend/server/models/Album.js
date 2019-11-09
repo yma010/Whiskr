64c61e55e263e2b235f934e3f0320ed2ab0ee164
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-09 15:57:55 -0500

    Add Mongoose hooks to Album

--- backend/server/models/Album.js
+++ backend/server/models/Album.js
@@ -21,4 +21,57 @@ const AlbumSchema = new Schema({
   }
 });
 
+// after first save: add to user, photos
+AlbumSchema.pre("save", async function() {
+  const album = this;
+  if (!album.isNew) return;
+
+  const User = mongoose.model("user");
+
+  await Promise.all([
+    User.findById(album.photographer)
+      .then(user => {
+        user.albums.push(album);
+        return user.save();
+      }),
+    album.populate("photos", (_, album) => (
+      Promise.all(album.photos.map(photo => {
+        photo.albums.push(album);
+        return photo.save();
+      }))
+    ))
+  ]);
+});
+
+// pre removal: remove from user, photos
+AlbumSchema.static("cleanUpBeforeRemoving", async function(album) {
+  const User = mongoose.model("user");
+
+  await Promise.all([
+    User.findById(album.photographer)
+      .then(user => {
+        user.albums.pull(album);
+        return user.save();
+      }),
+    album.populate("photos", (_, album) => (
+      Promise.all(album.photos.map(photo => {
+        photo.albums.pull(album);
+        return photo.save()
+      }))
+    ))
+  ]);
+});
+
+AlbumSchema.pre("remove", function() {
+  const album = this;
+  const Album = mongoose.model("album");
+  Album.cleanUpBeforeRemoving(album);
+});
+
+AlbumSchema.pre("deleteOne", async function() {
+  const Album = mongoose.model("album");
+  const album = await Album.findById(this.getFilter());
+  Album.cleanUpBeforeRemoving(album); 
+});
+
 module.exports = mongoose.model('album', AlbumSchema);
