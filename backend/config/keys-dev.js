2fedd14fc81fd2dc13176ff328f3189d735e832d
Marvin Ma <yma010@ucr.edu>
2019-11-09 16:45:42 -0800

    Deleted and Update Dev Keys

--- backend/config/keys-dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://whiskrAdmin:ldI585IJSL3dfpou@whiskr-gok6u.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "WOlXOZFEzuKO1OsjINne",
-  AWSAccessKey: "AKIA3GX4FT4VAAR5UD4I",
-  AWSSecretKey: "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",
-  AWSRegion: "us-west-1",
-  AWSUploadedFileURLLINK: "https://s3-us-west-1.amazonaws.com/whiskr-seeds"
-}
-
