19a59273ff8195be005a6da24a68ace3790fa5fb
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-10 10:05:03 -0500

    Debug SignUp component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,129 +1,59 @@
-import React, {useState} from "react";
-import {  SIGNUP_USER } from "../graphql/mutations";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import { useMutation } from '@apollo/react-hooks';
 
-function SignUp() {
-  let input;
-  const [inputs, setInputs] =useState({});
+import { SIGNUP_USER } from "../graphql/mutations";
+
+function SignUp(props) {
+  const [inputs, setInputs] = useState({
+    firstName: "",
+    lastName: "",
+    age: "",
+    email: "",
+    password: ""
+  });
   
-  const assignToken = (data) => {
-    const { token } = data.register;
+  const [signUpUser] = useMutation(
+    SIGNUP_USER, 
+    {
+      onCompleted: data => assignToken(data),
+      update: (client, data) => updateCache(client, data)
+    }
+  );
+
+  const assignToken = data => {
+    const { token } = data.signup;
     localStorage.setItem("auth-token", token);
-    this.props.history.push("/");
+    props.history.push("/");
   };
   
   const updateCache = (client, { data }) => {
     console.log(data);
     client.writeData({
-      data: { isLoggedIn: data.register.loggedIn }
+      data: { isLoggedIn: data.signup.loggedIn }
     });
-  }
+  };
   
-  const [SignUpUser, {data}] = useMutation(SIGNUP_USER, {
-    update: updateCache,
-    onCompleted: assignToken
-  });
-  
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     event.persist();
     setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
   };
   
-    return (
+  return (
     <form onSubmit={e => {
       e.preventDefault();
-      SignUpUser({ variables: { 
-        firstName: inputs.firstName,
-        lastName: inputs.lastName,
-        age: inputs.age,
-        email: inputs.email,
-        password: inputs.password
-      } });
-     
+      signUpUser({ 
+        variables: { ...inputs, age: parseInt(inputs.age) }
+      });
     }}>
       <input type="text" onChange={handleInputChange} name="firstName" value={inputs.firstName} placeholder="First name"/>
-        <input type="text" onChange={handleInputChange} name="lastName" value={inputs.lastName} placeholder="Last name"/>
-        <input type="text" onChange={handleInputChange} name="age" value={inputs.age} placeholder="Age"/>
-        <input type="text" onChange={handleInputChange} name="email"  values={inputs.email} placeholder="Email"/>
-        <input type="passsword" onChange={handleInputChange} name="password" values={inputs.password} placeholder="Password"/>
-      <button type="submit">Sign Up</button>
+      <input type="text" onChange={handleInputChange} name="lastName" value={inputs.lastName} placeholder="Last name"/>
+      <input type="number" onChange={handleInputChange} name="age" value={inputs.age} placeholder="Your age"/>
+      <input type="text" onChange={handleInputChange} name="email"  values={inputs.email} placeholder="Email address"/>
+      <input type="password" onChange={handleInputChange} name="password" values={inputs.password} placeholder="Password"/>
+      <button type="submit">Sign up</button>
     </form>
-  )
-
+  );
 }
 
-
-// class Register extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       email: "",
-//       name: "",
-//       password: ""
-//     };
-//   }
-
-//   update(field) {
-//     return e => this.setState({ [field]: e.target.value });
-//   }
-
-//   updateCache(client, { data }) {
-//     console.log(data);
-//     client.writeData({
-//       data: { isLoggedIn: data.register.loggedIn }
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <Mutation
-//         mutation={REGISTER_USER}
-//         onCompleted={data => {
-//           const { token } = data.register;
-//           localStorage.setItem("auth-token", token);
-//           this.props.history.push("/");
-//         }}
-//         update={(client, data) => this.updateCache(client, data)}
-//       >
-//         {registerUser => (
-//           <div>
-//             <form
-//               onSubmit={e => {
-//                 e.preventDefault();
-//                 registerUser({
-//                   variables: {
-//                     email: this.state.email,
-//                     password: this.state.password,
-//                     name: this.state.name
-//                   }
-//                 });
-//               }}
-//             >
-//               <input
-//                 value={this.state.email}
-//                 onChange={this.update("email")}
-//                 placeholder="Email"
-//               />
-//               <input
-//                 value={this.state.name}
-//                 onChange={this.update("name")}
-//                 placeholder="Name"
-//               />
-//               <input
-//                 value={this.state.password}
-//                 onChange={this.update("password")}
-//                 type="password"
-//                 placeholder="Password"
-//               />
-//               <button type="submit">Register</button>
-//             </form>
-//           </div>
-//         )}
-//       </Mutation>
-//     );
-//   }
-
-// }
-
-export default SignUp;
+export default withRouter(SignUp);
