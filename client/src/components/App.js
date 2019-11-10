525b112b54d03304a1dadab704b0329b98f3bbf4
FridaPolished <fridapulido@gmail.com>
2019-11-10 14:46:21 -0800

    Delete experimental navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import Login from './Login';
 import SignUp from './SignUp';
-import Navbar from './Navbar';
+
 
 const App = () => {
   return (
     <div>
       <h1>Whiskr</h1>
-        <Route path='/' component={Navbar} />
+       
         <Route exact path='/login' component={Login}/>
         <Route exact path='/signup' component={SignUp}/>
     </div>
