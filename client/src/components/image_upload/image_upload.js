acac771854fd08b310fe2f471fc54ea8c8b29186
Marvin Ma <yma010@ucr.edu>
2019-11-10 13:19:08 -0800

    Created Frontend Upload

--- client/src/components/image_upload/image_upload.js
+++ client/src/components/image_upload/image_upload.js
@@ -1,27 +1,4 @@
+import React from 'react';
+import axios from 'axios';
 
-import gql from 'graphql-tag'
-import { Mutation } from 'react-apollo'
-
-export const UPLOAD_FILE = gql`
-  mutation uploadFile($file: Upload!) {
-    uploadFile(file: $file) {
-      filename
-    }
-  }
-`;
-
-const uploadOneFile = () => {
-  return (   
-    <Mutation mutation={UPLOAD_FILE}>
-      {uploadFile => (
-        <input
-        type="file"
-        required
-        onChange={({ target: { validity, files: [file] } }) =>
-          validity.valid && uploadFile({ variables: { file } });
-        }
-       />
-      )}
-    </Mutation>
-  );
-};
+import { compose, gql, graphql } from 'react-apollo';
