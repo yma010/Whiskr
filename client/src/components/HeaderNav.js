c2d504778677c6b48c276587f352edc0be48bf5e
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-10 13:58:42 -0500

    Style left side of HeaderNav

--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -1,30 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "../stylesheets/header_nav.css";
+
 const HeaderNav = () => {
   return (
-    <div className="header-nav">
-      <div className="header-nav-left">
-        <Link to="/">whiskr</Link>
-        <ul className="header-nav-links">
-          <li>
-            <span>You</span>
-          </li>
-          <li>
-            <span>Explore</span>
-          </li>
-        </ul>
-      </div>
-      <div className="header-nav-right">
-        <div className="header-search-bar">
-          <i className="search-icon" />
-          <input type="text" placeholder="Cat photos" />
+    <div className="header-nav-container">
+      <header className="header-nav">
+        <div className="header-nav-left">
+          <Link to="/" className="whiskr-logo">
+            <span>whiskr</span>
+          </Link>
+          <ul className="header-nav-links">
+            <li>
+              <Link>You</Link>          {/* needs to be filled in */}
+            </li>
+            <li>
+              <Link>Explore</Link>      {/* needs to be filled in */}
+            </li>
+          </ul>
         </div>
-        <ul className="header-user-links">
-          <li className="header-upload-button" />
-          <li className="header-profile-button" />
-        </ul>
-      </div>
+        <div className="header-nav-right">
+          <div className="header-search-bar">
+            <i className="search-icon" />
+            <input type="text" placeholder="Cat photos" />
+          </div>
+          <ul className="header-user-links">
+            <li className="header-upload-button"><Link /></li>    {/* needs to be filled in */}
+            <li className="header-profile-button"><Link /></li>    {/* needs to be filled in */}
+          </ul>
+        </div>
+      </header>
     </div>
   );
 };
