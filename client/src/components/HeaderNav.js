bd298447e11798db2ade0f711ffcfb6c08300bf1
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-10 17:23:45 -0500

    Add and style HeaderNav's lefthand dropdowns

--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../stylesheets/header_nav.css";
 
 const HeaderNav = () => {
+  const [isYouDropdownOpen, setYouDropdownOpen] = useState(false);
+  const [isExploreDropdownOpen, setExploreDropdownOpen] = useState(false);
+
   return (
     <div className="header-nav-container">
       <header className="header-nav">
@@ -12,15 +15,31 @@ const HeaderNav = () => {
             <span>whiskr</span>
           </Link>
           <ul className="header-nav-links">
-            <li>
-              <Link>                    {/* needs to be filled in */}
-                <span>You</span>
-              </Link>
+            <li 
+              onMouseEnter={() => setYouDropdownOpen(true)}
+              onMouseLeave={() => setYouDropdownOpen(false)}
+            >
+              <Link><span>You</span></Link>               {/* link needs to be filled in */}
+              {isYouDropdownOpen ? (
+                <ul className="header-dropdown">
+                  <li><Link>Photostream</Link></li>       {/* link needs to be filled in */}
+                  <li><Link>Albums</Link></li>            {/* link needs to be filled in */}
+                  <li><Link>Faves</Link></li>             {/* link needs to be filled in */}
+                  <li><Link>Camera Roll</Link></li>       {/* link needs to be filled in */}
+                </ul>
+              ) : ""}
             </li>
-            <li>
-              <Link>                    {/* needs to be filled in */}
-                <span>Explore</span>
-              </Link>      
+            <li
+              onMouseEnter={() => setExploreDropdownOpen(true)}
+              onMouseLeave={() => setExploreDropdownOpen(false)}
+            >
+              <Link><span>Explore</span></Link>      {/* link needs to be filled in */}
+              {isExploreDropdownOpen ? (
+                <ul className="header-dropdown">
+                  <li><Link>Recent Photos</Link></li>       {/* link needs to be filled in */}
+                  <li><Link>Trending</Link></li>            {/* link needs to be filled in */}
+                </ul>
+              ) : ""}
             </li>
           </ul>
         </div>
@@ -29,8 +48,8 @@ const HeaderNav = () => {
             <i className="search-icon" />
             <input type="text" placeholder="Cat photos" />
           </form>
-          <li className="header-upload-button"><Link /></li>    {/* needs to be filled in */}
-          <li className="header-avatar-button"><button /></li>    {/* needs to be filled in */}
+          <li className="header-upload-button"><Link /></li>    {/* link needs to be filled in */}
+          <li className="header-avatar-button"><button /></li>    {/* link needs to be filled in */}
         </ul>
       </header>
     </div>
