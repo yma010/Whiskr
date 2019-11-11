c57793efe645e8b212a1ba5e488641fa09e6fb7c
Marvin Ma <yma010@ucr.edu>
2019-11-11 01:04:54 -0800

    Added typeDefs and Resolvers in server

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,14 +3,92 @@ const express = require("express"),
       bodyParser = require("body-parser"),
       expressGraphQL = require("express-graphql"),
       cors = require("cors"),
-      keys = require('../config/keys');
+      keys = require('../config/keys'),
+      AWS = require('aws-sdk'),
+      fs = require('fs'),
       graphQL = require('graphql');
 
+const { ApolloServer, gql } = require('apollo-server');
 const db = require("../config/keys").mongoURI;
-const models = require("./models/index");
-const schema = require("./schema/schema");
-
+const models = require("./models/index"),
+      schema = require("./schema/schema");
+      
 const app = express();
+
+const typeDefs = gql `
+  type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
+
+  type Query {
+    _ : Boolean
+  }
+
+  type Mutation {
+    singleUpload(file: Upload!): File!,
+    singleUploadStream(file: Upload!): File!
+  }
+`;
+
+const resolvers = {
+  Mutation: {
+    singleUpload: (parent, arg) => {
+      return args.file.then(file => {
+        const {
+          createReadStream,
+          filename,
+          mimetype
+        } = file
+
+        const fileStream = createReadStream()
+
+        fileStream.pipe(fs.createWriteStream(`./uploadedFiles/${filename}`))
+
+        return file;
+      })
+    },
+
+    singleUploadStream: async (parent, args) => {
+      const file = await args.file
+      const {
+        createReadStream,
+        filename,
+        mimetype
+      } = file
+      const fileStream = createReadStream()
+
+      const uploadParams = {
+        Bucket: 'whiskr-seeds',
+        Key: filename,
+        Body: fileStream
+      };
+      const result = await s3.upload(uploadParams).promise()
+
+      console.log(result)
+
+      return file;
+    }
+  }
+};
+
+
+const AWSConfig = AWS.config.update({
+  secretAccessKey: keys.AWSSecretKey,
+  accessKeyId: keys.AWSAccessKey,
+  region: keys.AWSRegion
+});
+
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01'
+});
+
+const server = new ApolloServer({ typeDefs, resolvers});
+
+server.listen().then(({ url }) => {
+  console.log(`\`🚀  Server ready at ${url}`);
+})
 
 if (!db) {
   throw new Error("You must provide a string to connect to MongoDB Atlas");
--- client/src/components/image_upload/image_upload.js
+++ client/src/components/image_upload/image_upload.js
@@ -1,4 +1,74 @@
 import React from 'react';
 import axios from 'axios';
-
+import moment from 'moment';
 import { compose, gql, graphql } from 'react-apollo';
+
+class Upload extends React.Component {
+  state = {
+    title = "",
+    file: null
+  }
+
+
+  onDrop = async files => {
+    this.setState({ files: files[0]});
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  uploadToS3 = async (file, signedRequest) => {
+    const options = {
+      headers: {
+        "Content-Type": file.type
+      }
+    };
+    await axios.put(signedRequest, file, options);
+  }
+
+  formatFileName = filename => {
+    const date = moment().format("YYYYMMDD");
+    const randString = Math.random()
+      .toString(36)
+      .substring(2, 7);
+    const cleanFilename = filename.toLowerCase().replace(/[^a-z0-9]/g, "-"); //regexp here states to replace all spaces with a "-"
+    const newFilename = `images/${date}-${randString}-${cleanFilename}`;
+    return newFilename.substring(0,60);
+  };
+
+  submit = async () => {
+    const {name, file} = this.state;
+    const resp = await this.props.s3Sign({
+      variables: {
+        filename: this.formatFileName(file.name),
+        filetype: file.type
+      }
+    });
+
+    const { signedRequest, url } = response.data.signS3;
+    await this.uploadToS3(file, signedRequest);
+
+    const graphQLResp = await this.props.newPhoto({
+      variables: {
+        title,
+        imageURL: url
+      }
+    });
+  }
+
+  render()   {
+    return (
+      <div>
+        <input name="title" onChange={this.onChange} value={this.state.title} />
+          <Dropzone onDrop={this.onDrop}>
+            <p>You can upload 1000 more photos and videos.</p>
+            <p> Drag & drop photos and videos here</p>
+          </Dropzone>
+      </div>
+    )
+  }
+
+};
