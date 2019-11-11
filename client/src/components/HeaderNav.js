bf2a6b4594138f1d1840f23bda5101d729cb0b50
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-10 23:09:02 -0500

    Refactor HeaderNav; adjust based on isLoggedIn

--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -1,61 +1,95 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
 
+import { IS_LOGGED_IN, CURRENT_USER } from "../graphql/queries";
 import "../stylesheets/header_nav.css";
 
 const HeaderNav = () => {
   const [isYouDropdownOpen, setYouDropdownOpen] = useState(false);
   const [isExploreDropdownOpen, setExploreDropdownOpen] = useState(false);
   const [isSearchFocused, setSearchFocus] = useState(false);
+  const [isProfileDropdownOpen, setProfileDropdownOpen] = useState(false);
+  const { data: { isLoggedIn, currentUserId } } = useQuery(IS_LOGGED_IN);
+  
+  let firstName;
+  const { data: currentUser } = useQuery(CURRENT_USER, {
+    variables: { _id: currentUserId }
+  });
+  if (currentUser) {
+    firstName = currentUser.firstName;
+  }
+
+  const youDropdown = (
+    <ul className="header-dropdown">
+      <li><Link>Photostream</Link></li>       {/* link needs to be filled in */}
+      <li><Link>Albums</Link></li>            {/* link needs to be filled in */}
+      <li><Link>Faves</Link></li>             {/* link needs to be filled in */}
+      <li><Link>Camera Roll</Link></li>       {/* link needs to be filled in */}
+    </ul>
+  );
+
+  const exploreDropdown = (
+    <ul className="header-dropdown">
+      <li><Link>Recent Photos</Link></li>       {/* link needs to be filled in */}
+      <li><Link>Trending</Link></li>            {/* link needs to be filled in */}
+    </ul>
+  );
+
+  const searchBar = (
+    <form className={isSearchFocused ? "header-search-bar focused" : "header-search-bar"}>
+      <button className="search-icon" />
+      <input
+        onFocus={() => setSearchFocus(true)}
+        onBlur={() => setSearchFocus(false)}
+        type="text"
+        placeholder="Cat photos"
+      />
+    </form>
+  );
+
+  const sessionLinks = (
+    <ul className="header-session-links">
+      <li><Link to="/login">Log In</Link></li>
+      <li><Link to="/signup">Sign Up</Link></li>
+    </ul>
+  );
 
   return (
     <div className="header-nav-container">
       <header className="header-nav">
         <div className="header-nav-left">
-          <Link to="/" className="whiskr-logo">
-            <span>whiskr</span>
-          </Link>
+          <Link to="/" className="whiskr-logo"><span>whiskr</span></Link>
           <ul className="header-nav-links">
+            {isLoggedIn ? (
             <li 
               onMouseEnter={() => setYouDropdownOpen(true)}
               onMouseLeave={() => setYouDropdownOpen(false)}
             >
               <Link><span>You</span></Link>               {/* link needs to be filled in */}
-              {isYouDropdownOpen ? (
-                <ul className="header-dropdown">
-                  <li><Link>Photostream</Link></li>       {/* link needs to be filled in */}
-                  <li><Link>Albums</Link></li>            {/* link needs to be filled in */}
-                  <li><Link>Faves</Link></li>             {/* link needs to be filled in */}
-                  <li><Link>Camera Roll</Link></li>       {/* link needs to be filled in */}
-                </ul>
-              ) : ""}
-            </li>
+              {isYouDropdownOpen ? youDropdown : ""}
+            </li>) : ""}
             <li
               onMouseEnter={() => setExploreDropdownOpen(true)}
               onMouseLeave={() => setExploreDropdownOpen(false)}
             >
               <Link><span>Explore</span></Link>      {/* link needs to be filled in */}
-              {isExploreDropdownOpen ? (
-                <ul className="header-dropdown">
-                  <li><Link>Recent Photos</Link></li>       {/* link needs to be filled in */}
-                  <li><Link>Trending</Link></li>            {/* link needs to be filled in */}
-                </ul>
-              ) : ""}
+              {isExploreDropdownOpen ? exploreDropdown : ""}
             </li>
           </ul>
         </div>
         <ul className="header-nav-right">
-          <form className={isSearchFocused ? "header-search-bar focused" : "header-search-bar"}>
-            <button className="search-icon" />
-            <input 
-              onFocus={() => setSearchFocus(true)}
-              onBlur={() => setSearchFocus(false)}
-              type="text" 
-              placeholder="Cat photos" 
-            />
-          </form>
+          <li>{searchBar}</li>
           <li className="header-upload-button"><Link /></li>    {/* link needs to be filled in */}
-          <li className="header-avatar-button"><button /></li>    {/* link needs to be filled in */}
+          {isLoggedIn ? (
+          <li className="header-avatar-button">
+            <button onClick={() => setProfileDropdownOpen(!isProfileDropdownOpen)}/>
+            {/* {isprofileDropdownOpen ? 
+            
+            ): "" } */}
+          </li>
+          ) : (
+          <li>{sessionLinks}</li>)}
         </ul>
       </header>
     </div>
