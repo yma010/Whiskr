808bba9f4d6d0a4cdebac542604e0a25fe93d744
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-13 13:50:44 -0500

    Style columns in photo index

--- client/src/components/photos/PhotoIndex.js
+++ client/src/components/photos/PhotoIndex.js
@@ -15,42 +15,50 @@ function PhotoIndex() {
     return <div>Error!</div>
   }
 
-  let photos;
-  photos = data.photos.map(photo => {
-    return (
-      <div className="single-card" key={photo._id}>
-        <li key={photo._id}>
+  const photoIndexItem = photo => (
+    <div className="single-card" key={photo._id}>
+      <li key={photo._id}>
         <div className="card-identity">
-          <img className="user-avatar" src={photo.photographer.avatarURL} alt="avatar"/>
-          <Link to={`/users/${photo.photographer._id}`}> 
-            {photo.photographer.firstName} {photo.photographer.lastName} 
-          </Link> 
-        </div>        
+          <img className="user-avatar" src={photo.photographer.avatarURL} alt="avatar" />
+          <Link to={`/users/${photo.photographer._id}`}>
+            {photo.photographer.firstName} {photo.photographer.lastName}
+          </Link>
+        </div>
         <div className="card-photo">
-          <Link to={`/photos/${photo._id}`} title={photo.description}> 
-            <img src={photo.imageURL} alt={photo.description}></img> 
+          <Link to={`/photos/${photo._id}`} title={photo.description}>
+            <img src={photo.imageURL} alt={photo.description}></img>
           </Link>
         </div>
         <div className="card-info">
           <div className="photo-title">
-              <p>{photo.title}</p>
+            <p>{photo.title}</p>
           </div>
           <div className="photo-actions">
             <div className="activity-card-counts">
-              <span className="view-count">{photo.views} Views</span> 
+              <span className="view-count">{photo.views} Views</span>
             </div>
           </div>
-        </div> 
-        </li> 
-      </div>
-    );
+        </div>
+      </li>
+    </div>
+  );
+  
+  const leftColumnPhotos = [], rightColumnPhotos = [];
+  
+  data.photos.forEach((photo, idx) => {
+    idx % 2 === 0 ? 
+      leftColumnPhotos.push(photoIndexItem(photo))
+      : rightColumnPhotos.push(photoIndexItem(photo));
   });
 
   return (
     <div className="feed-container">
       <div className="feed">
         <ul>
-          {photos}
+          {leftColumnPhotos}
+        </ul>
+        <ul>
+          {rightColumnPhotos}
         </ul>
       </div>
     </div>
