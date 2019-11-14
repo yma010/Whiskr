dad6e1cb50d000898749fd35097c0694560c0d3d
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-14 10:37:43 -0500

    Add photo info to lightbox

--- client/src/components/photos/PhotoIndexItem.js
+++ client/src/components/photos/PhotoIndexItem.js
@@ -7,15 +7,50 @@ const PhotoIndexItem = ({ photo, setNoScroll }) => {
   const viewWidth = window.innerWidth;
 
   const [isDisplayed, setIsDisplayed] = useState(false);
+  const [isInfoDisplayed, setInfoDisplayed] = useState(false);
+
+  const cardIdentity = (
+    <div className="card-identity">
+      <Link
+        className="user-avatar"
+        to={`/users/${photo.photographer._id}`}
+        style={{ backgroundImage: `url(${photo.photographer.avatarURL || '../../public/camera-avatar.png'})` }}
+      />
+      <div>
+        <Link to={`/users/${photo.photographer._id}`}>
+          {photo.photographer.firstName} {photo.photographer.lastName}
+        </Link>
+        <span>Featured</span>
+      </div>
+    </div>
+  );
+
+  const cardInfo = (
+    <div className="card-info">
+      <div className="photo-title-description">
+        <p>{photo.title}</p>
+        <p>{photo.description}</p>
+      </div>
+      <div className="photo-actions">
+        <div className="activity-card-counts">
+          <span className="view-count">{photo.views} Views</span>
+          <span>{photo.comments.length} Comments</span>
+        </div>
+        <div className="photo-actions-links">
+          <i className="photo-actions-comment-icon" />
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <li className="single-card">
-      <div className={isDisplayed ? "photo-lightbox" : "photo-lightbox-hidden"}>
+      <div className={isDisplayed ? "photo-lightbox" : "photo-lightbox hidden"}>
         <button 
           className="close-photo-lightbox"
           onClick={() => { setIsDisplayed(false); setNoScroll(false); }}
         />
-        {isDisplayed ? (<Link 
+        {isDisplayed ? (<div 
           to={`/photos/${photo._id}`} title={photo.description}
           className="photo-lightbox-image"
           style={{ 
@@ -23,21 +58,17 @@ const PhotoIndexItem = ({ photo, setNoScroll }) => {
             height: img.current.offsetHeight / 480 * .85 * viewWidth > 0.85 * viewHeight ? "85vh" : `${img.current.offsetHeight / 480 * .85 * viewWidth}px`,
             width: img.current.offsetHeight / 480 * .85 * viewWidth > 0.85 * viewHeight ? `${480 / img.current.offsetHeight * .85 * viewHeight}px` : "85vw"
           }}
-        />) : ""}
+          onMouseEnter={() => setInfoDisplayed(true)}
+          onMouseLeave={() => setInfoDisplayed(false)}
+        >
+          <div className={isInfoDisplayed ? "photo-lightbox-info" : "photo-lightbox-info hidden"}>
+            <Link to={`/photos/${photo._id}`} title={photo.description} className="photo-lightbox-link"/>
+            {cardIdentity}
+            {cardInfo}
+          </div>
+        </div>) : ""}
       </div>
-      <div className="card-identity">
-        <Link
-          className="user-avatar"
-          to={`/users/${photo.photographer._id}`}
-          style={{ backgroundImage: `url(${photo.photographer.avatarURL || '../../public/camera-avatar.png'})` }}
-        />
-        <div>
-          <Link to={`/users/${photo.photographer._id}`}>
-            {photo.photographer.firstName} {photo.photographer.lastName}
-          </Link>
-          <span>Featured</span>
-        </div>
-      </div>
+      {cardIdentity}
       <button 
         className="card-photo" 
         title={photo.description} 
@@ -45,20 +76,7 @@ const PhotoIndexItem = ({ photo, setNoScroll }) => {
       >
         <img src={photo.imageURL} alt={photo.description} ref={img}></img>
       </button>
-      <div className="card-info">
-        <div className="photo-title">
-          <p>{photo.title}</p>
-        </div>
-        <div className="photo-actions">
-          <div className="activity-card-counts">
-            <span className="view-count">{photo.views} Views</span>
-            <span>{photo.comments.length} Comments</span>
-          </div>
-          <div className="photo-actions-links">
-            <i className="photo-actions-comment-icon" />
-          </div>
-        </div>
-      </div>
+      {cardInfo}
     </li>
   );
 };
