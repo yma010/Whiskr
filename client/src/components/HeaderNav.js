8d767d1d05a1d3a6a28081ce4f2234cb4dd02e85
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-17 17:59:25 -0500

    Fix issue with logged out user

--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -20,14 +20,14 @@ const HeaderNav = props => {
     }
   });
 
-  const youDropdown = (
+  const youDropdown = currentUser ? (
     <ul className="header-dropdown">
       <li><Link to={`/users/${currentUser._id}/photos`}>Photostream</Link></li>
       <li><Link>Albums</Link></li>            {/* link needs to be filled in */}
       <li><Link>Faves</Link></li>             {/* link needs to be filled in */}
       <li><Link>Camera Roll</Link></li>       {/* link needs to be filled in */}
     </ul>
-  );
+  ) : "";
 
   const exploreDropdown = (
     <ul className="header-dropdown">
